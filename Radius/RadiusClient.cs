3d8e5793b60becc66d4465660ab6438ce3e75c50
Niklas Hedlund <nojan1989@gmail.com>
2016-02-15 17:34:42 +0100

    Removed Connect call in favor of passing IPEndPoint directly to SendAsync.

--- Radius/RadiusClient.cs
+++ Radius/RadiusClient.cs
@@ -72,7 +72,9 @@ namespace FP.Radius
 			{
 				udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, _SocketTimeout);
 
-				try
+                IPAddress hostIP = null;
+
+                try
 				{
 					// Starting with Vista, we are able to bind to a local endpoint to guarantee the packet
 					// will be sent out a particular interface
@@ -81,12 +83,16 @@ namespace FP.Radius
 					if(_LocalEndPoint != null)
 						udpClient.Client.Bind(_LocalEndPoint);
 					
-					IPAddress hostIP;
-					if (IPAddress.TryParse(_HostName, out hostIP))
-						udpClient.Connect(hostIP, (int) _AuthPort);
-					else
-						udpClient.Connect(_HostName, (int) _AuthPort);
-					
+					if(!IPAddress.TryParse(_HostName, out hostIP))
+                    {
+                        //Try performing a DNS lookup
+                        var host = Dns.GetHostEntry(_HostName);
+                        var ipv4Addresses = host.AddressList.Where(a => a.AddressFamily == AddressFamily.InterNetwork);
+                        if (ipv4Addresses.Count() == 0)
+                            throw new Exception("Resolving " + _HostName + " returned no hits in DNS");
+
+                        hostIP = ipv4Addresses.First();
+                    }
 				}
 				catch (SocketException e)
 				{
@@ -98,13 +104,12 @@ namespace FP.Radius
 						return null;
 				}
 
-				var endPoint = (IPEndPoint)udpClient.Client.RemoteEndPoint;
-
+				var endPoint = new IPEndPoint(hostIP, (int)_AuthPort);
 				int numberOfAttempts = 0;
 
 				do
 				{
-					await udpClient.SendAsync(packet.RawData, packet.RawData.Length);
+					await udpClient.SendAsync(packet.RawData, packet.RawData.Length, endPoint);
 
 					try
 					{
