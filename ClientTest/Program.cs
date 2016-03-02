176a695c127967eab602403aecca657b4b68122d
Shane Powser <spowser@frontporch.com>
2016-03-02 10:32:55 -0800

    Make all spacing tabs

--- ClientTest/Program.cs
+++ ClientTest/Program.cs
@@ -5,68 +5,68 @@ using System.Threading.Tasks;
 
 namespace ClientTest
 {
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            if (args.Length != 4)
-            {
-                ShowUsage();
-                return;
-            }
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			if (args.Length != 4)
+			{
+				ShowUsage();
+				return;
+			}
 
-            //args = new string[4];
-            //args[0] = "192.168.1.1";
-            //args[1] = "secret";
-            //args[2] = "username";
-            //args[3] = "password";
+			//args = new string[4];
+			//args[0] = "192.168.1.1";
+			//args[1] = "secret";
+			//args[2] = "username";
+			//args[3] = "password";
 
-            try
-            {
-                Authenticate(args).Wait();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+			try
+			{
+				Authenticate(args).Wait();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
 
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadLine();
-        }
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadLine();
+		}
 
-        private async static Task Authenticate(string[] args)
-        {
-            RadiusClient rc = new RadiusClient(args[0], args[1]);
-            RadiusPacket authPacket = rc.Authenticate(args[2], args[3]);
-            authPacket.SetAttribute(new VendorSpecificAttribute(10135, 1, UTF8Encoding.UTF8.GetBytes("Testing")));
-            authPacket.SetAttribute(new VendorSpecificAttribute(10135, 2, new[] { (byte)7 }));
-            RadiusPacket receivedPacket = await rc.SendAndReceivePacket(authPacket);
-            if (receivedPacket == null) throw new Exception("Can't contact remote radius server !");
+		private async static Task Authenticate(string[] args)
+		{
+			RadiusClient rc = new RadiusClient(args[0], args[1]);
+			RadiusPacket authPacket = rc.Authenticate(args[2], args[3]);
+			authPacket.SetAttribute(new VendorSpecificAttribute(10135, 1, UTF8Encoding.UTF8.GetBytes("Testing")));
+			authPacket.SetAttribute(new VendorSpecificAttribute(10135, 2, new[] { (byte)7 }));
+			RadiusPacket receivedPacket = await rc.SendAndReceivePacket(authPacket);
+			if (receivedPacket == null) throw new Exception("Can't contact remote radius server !");
 
-            switch (receivedPacket.PacketType)
-            {
-                case RadiusCode.ACCESS_ACCEPT:
-                    Console.WriteLine("Access-Accept");
-                    foreach (var attr in receivedPacket.Attributes)
-                        Console.WriteLine(attr.Type.ToString() + " = " + attr.Value);
-                    break;
-                case RadiusCode.ACCESS_CHALLENGE:
-                    Console.WriteLine("Access-Challenge");
-                    break;
-                case RadiusCode.ACCESS_REJECT:
-                    Console.WriteLine("Access-Reject");
-                    if (!rc.VerifyAuthenticator(authPacket, receivedPacket))
-                        Console.WriteLine("Authenticator check failed: Check your secret");
-                    break;
-                default:
-                    Console.WriteLine("Rejected");
-                    break;
-            }
-        }
+			switch (receivedPacket.PacketType)
+			{
+				case RadiusCode.ACCESS_ACCEPT:
+					Console.WriteLine("Access-Accept");
+					foreach (var attr in receivedPacket.Attributes)
+						Console.WriteLine(attr.Type.ToString() + " = " + attr.Value);
+					break;
+				case RadiusCode.ACCESS_CHALLENGE:
+					Console.WriteLine("Access-Challenge");
+					break;
+				case RadiusCode.ACCESS_REJECT:
+					Console.WriteLine("Access-Reject");
+					if (!rc.VerifyAuthenticator(authPacket, receivedPacket))
+						Console.WriteLine("Authenticator check failed: Check your secret");
+					break;
+				default:
+					Console.WriteLine("Rejected");
+					break;
+			}
+		}
 
-        private static void ShowUsage()
-        {
-            Console.WriteLine("Usage : ClientTest.exe hostname sharedsecret username password");
-        }
-    }
+		private static void ShowUsage()
+		{
+			Console.WriteLine("Usage : ClientTest.exe hostname sharedsecret username password");
+		}
+	}
 }
--- Radius/RadiusClient.cs
+++ Radius/RadiusClient.cs
@@ -9,153 +9,153 @@ using System.Threading.Tasks;
 
 namespace FP.Radius
 {
-    public class RadiusClient
-    {
-        #region Constants
-        private const int DEFAULT_RETRIES = 3;
-        private const uint DEFAULT_AUTH_PORT = 1812;
-        private const uint DEFAULT_ACCT_PORT = 1813;
-        private const int DEFAULT_SOCKET_TIMEOUT = 3000;
-        #endregion
+	public class RadiusClient
+	{
+		#region Constants
+		private const int DEFAULT_RETRIES = 3;
+		private const uint DEFAULT_AUTH_PORT = 1812;
+		private const uint DEFAULT_ACCT_PORT = 1813;
+		private const int DEFAULT_SOCKET_TIMEOUT = 3000;
+		#endregion
 
-        #region Private
-        private string _SharedSecret = String.Empty;
-        private string _HostName = String.Empty;
-        private uint _AuthPort = DEFAULT_AUTH_PORT;
-        private uint _AcctPort = DEFAULT_ACCT_PORT;
-        private uint _AuthRetries = DEFAULT_RETRIES;
-        private uint _AcctRetries = DEFAULT_RETRIES;
-        private int _SocketTimeout = DEFAULT_SOCKET_TIMEOUT;
-        private IPEndPoint _LocalEndPoint;
-        #endregion
+		#region Private
+		private string _SharedSecret = String.Empty;
+		private string _HostName = String.Empty;
+		private uint _AuthPort = DEFAULT_AUTH_PORT;
+		private uint _AcctPort = DEFAULT_ACCT_PORT;
+		private uint _AuthRetries = DEFAULT_RETRIES;
+		private uint _AcctRetries = DEFAULT_RETRIES;
+		private int _SocketTimeout = DEFAULT_SOCKET_TIMEOUT;
+		private IPEndPoint _LocalEndPoint;
+		#endregion
 
-        #region Properties
-        public int SocketTimeout
-        {
-            get { return _SocketTimeout; }
-            set { _SocketTimeout = value; }
-        }
-        #endregion
+		#region Properties
+		public int SocketTimeout
+		{
+			get { return _SocketTimeout; }
+			set { _SocketTimeout = value; }
+		}
+		#endregion
 
-        #region Constructors
-        public RadiusClient(string hostName, string sharedSecret,
-                            int sockTimeout = DEFAULT_SOCKET_TIMEOUT,
-                            uint authPort = DEFAULT_AUTH_PORT,
-                            uint acctPort = DEFAULT_ACCT_PORT,
-                            IPEndPoint localEndPoint = null)
-        {
-            _HostName = hostName;
-            _AuthPort = authPort;
-            _AcctPort = acctPort;
-            _LocalEndPoint = localEndPoint;
-            _SharedSecret = sharedSecret;
-            _SocketTimeout = sockTimeout;
-        }
-        #endregion
+		#region Constructors
+		public RadiusClient(string hostName, string sharedSecret,
+							int sockTimeout = DEFAULT_SOCKET_TIMEOUT,
+							uint authPort = DEFAULT_AUTH_PORT,
+							uint acctPort = DEFAULT_ACCT_PORT,
+							IPEndPoint localEndPoint = null)
+		{
+			_HostName = hostName;
+			_AuthPort = authPort;
+			_AcctPort = acctPort;
+			_LocalEndPoint = localEndPoint;
+			_SharedSecret = sharedSecret;
+			_SocketTimeout = sockTimeout;
+		}
+		#endregion
 
-        #region Public Methods
-        public RadiusPacket Authenticate(string username, string password)
-        {
-            RadiusPacket packet = new RadiusPacket(RadiusCode.ACCESS_REQUEST);
-            packet.SetAuthenticator(_SharedSecret);
-            byte[] encryptedPass = Utils.EncodePapPassword(Encoding.ASCII.GetBytes(password), packet.Authenticator, _SharedSecret);
-            packet.SetAttribute(new RadiusAttribute(RadiusAttributeType.USER_NAME, Encoding.ASCII.GetBytes(username)));
-            packet.SetAttribute(new RadiusAttribute(RadiusAttributeType.USER_PASSWORD, encryptedPass));
-            return packet;
-        }
+		#region Public Methods
+		public RadiusPacket Authenticate(string username, string password)
+		{
+			RadiusPacket packet = new RadiusPacket(RadiusCode.ACCESS_REQUEST);
+			packet.SetAuthenticator(_SharedSecret);
+			byte[] encryptedPass = Utils.EncodePapPassword(Encoding.ASCII.GetBytes(password), packet.Authenticator, _SharedSecret);
+			packet.SetAttribute(new RadiusAttribute(RadiusAttributeType.USER_NAME, Encoding.ASCII.GetBytes(username)));
+			packet.SetAttribute(new RadiusAttribute(RadiusAttributeType.USER_PASSWORD, encryptedPass));
+			return packet;
+		}
 
-        public async Task<RadiusPacket> SendAndReceivePacket(RadiusPacket packet, int retries = DEFAULT_RETRIES)
-        {
-            using (UdpClient udpClient = new UdpClient())
-            {
-                udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, _SocketTimeout);
+		public async Task<RadiusPacket> SendAndReceivePacket(RadiusPacket packet, int retries = DEFAULT_RETRIES)
+		{
+			using (UdpClient udpClient = new UdpClient())
+			{
+				udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, _SocketTimeout);
 
-                IPAddress hostIP = null;
+				IPAddress hostIP = null;
 
-                try
-                {
-                    // Starting with Vista, we are able to bind to a local endpoint to guarantee the packet
-                    // will be sent out a particular interface
-                    // This is explained in the following blog
-                    // http://blogs.technet.com/b/networking/archive/2009/04/25/source-ip-address-selection-on-a-multi-homed-windows-computer.aspx
-                    if (_LocalEndPoint != null)
-                        udpClient.Client.Bind(_LocalEndPoint);
+				try
+				{
+					// Starting with Vista, we are able to bind to a local endpoint to guarantee the packet
+					// will be sent out a particular interface
+					// This is explained in the following blog
+					// http://blogs.technet.com/b/networking/archive/2009/04/25/source-ip-address-selection-on-a-multi-homed-windows-computer.aspx
+					if (_LocalEndPoint != null)
+						udpClient.Client.Bind(_LocalEndPoint);
 
-                    if (!IPAddress.TryParse(_HostName, out hostIP))
-                    {
-                        //Try performing a DNS lookup
-                        var host = Dns.GetHostEntry(_HostName);
-                        hostIP = host.AddressList.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);
-                        if (hostIP == null)
-                            throw new Exception("Resolving " + _HostName + " returned no hits in DNS");
+					if (!IPAddress.TryParse(_HostName, out hostIP))
+					{
+						//Try performing a DNS lookup
+						var host = Dns.GetHostEntry(_HostName);
+						hostIP = host.AddressList.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);
+						if (hostIP == null)
+							throw new Exception("Resolving " + _HostName + " returned no hits in DNS");
 
-                    }
-                }
-                catch (SocketException e)
-                {
-                    int hr = Marshal.GetHRForException(e);
-                    string hexValue = hr.ToString("X");
+					}
+				}
+				catch (SocketException e)
+				{
+					int hr = Marshal.GetHRForException(e);
+					string hexValue = hr.ToString("X");
 
-                    //The requested name is valid, but no data of the requested type was found
-                    if (hexValue == "80004005")
-                        return null;
-                }
+					//The requested name is valid, but no data of the requested type was found
+					if (hexValue == "80004005")
+						return null;
+				}
 
-                var endPoint = new IPEndPoint(hostIP, (int)_AuthPort);
-                int numberOfAttempts = 0;
+				var endPoint = new IPEndPoint(hostIP, (int)_AuthPort);
+				int numberOfAttempts = 0;
 
-                do
-                {
-                    try
-                    {
-                        await udpClient.SendAsync(packet.RawData, packet.RawData.Length, endPoint);
+				do
+				{
+					try
+					{
+						await udpClient.SendAsync(packet.RawData, packet.RawData.Length, endPoint);
 
-                        // Using the synchronous method for the timeout features
-                        var result = udpClient.Receive(ref endPoint);
-                        RadiusPacket receivedPacket = new RadiusPacket(result);
+						// Using the synchronous method for the timeout features
+						var result = udpClient.Receive(ref endPoint);
+						RadiusPacket receivedPacket = new RadiusPacket(result);
 
-                        if (receivedPacket.Valid)
-                            return receivedPacket;
-                    }
-                    catch (SocketException)
-                    {
-                        //Server isn't responding
-                    }
+						if (receivedPacket.Valid)
+							return receivedPacket;
+					}
+					catch (SocketException)
+					{
+						//Server isn't responding
+					}
 
-                    numberOfAttempts++;
+					numberOfAttempts++;
 
-                } while (numberOfAttempts < retries);
-            }
+				} while (numberOfAttempts < retries);
+			}
 
-            return null;
-        }
-        #endregion
+			return null;
+		}
+		#endregion
 
-        #region Private Methods
-        public bool VerifyAuthenticator(RadiusPacket requestedPacket, RadiusPacket receivedPacket)
-        {
-            return requestedPacket.Identifier == receivedPacket.Identifier
-                && receivedPacket.Authenticator.SequenceEqual(Utils.ResponseAuthenticator(receivedPacket.RawData, requestedPacket.Authenticator, _SharedSecret));
-        }
+		#region Private Methods
+		public bool VerifyAuthenticator(RadiusPacket requestedPacket, RadiusPacket receivedPacket)
+		{
+			return requestedPacket.Identifier == receivedPacket.Identifier
+				&& receivedPacket.Authenticator.SequenceEqual(Utils.ResponseAuthenticator(receivedPacket.RawData, requestedPacket.Authenticator, _SharedSecret));
+		}
 
-        public static bool VerifyAccountingAuthenticator(byte[] radiusPacket, string secret)
-        {
-            var secretBytes = Encoding.ASCII.GetBytes(secret);
+		public static bool VerifyAccountingAuthenticator(byte[] radiusPacket, string secret)
+		{
+			var secretBytes = Encoding.ASCII.GetBytes(secret);
 
-            byte[] sum = new byte[radiusPacket.Length + secretBytes.Length];
+			byte[] sum = new byte[radiusPacket.Length + secretBytes.Length];
 
-            byte[] authenticator = new byte[16];
-            Array.Copy(radiusPacket, 4, authenticator, 0, 16);
+			byte[] authenticator = new byte[16];
+			Array.Copy(radiusPacket, 4, authenticator, 0, 16);
 
-            Array.Copy(radiusPacket, 0, sum, 0, radiusPacket.Length);
-            Array.Copy(secretBytes, 0, sum, radiusPacket.Length, secretBytes.Length);
-            Array.Clear(sum, 4, 16);
+			Array.Copy(radiusPacket, 0, sum, 0, radiusPacket.Length);
+			Array.Copy(secretBytes, 0, sum, radiusPacket.Length, secretBytes.Length);
+			Array.Clear(sum, 4, 16);
 
-            MD5 md5 = new MD5CryptoServiceProvider();
+			MD5 md5 = new MD5CryptoServiceProvider();
 
-            var hash = md5.ComputeHash(sum, 0, sum.Length);
-            return authenticator.SequenceEqual(hash);
-        }
-        #endregion
-    }
+			var hash = md5.ComputeHash(sum, 0, sum.Length);
+			return authenticator.SequenceEqual(hash);
+		}
+		#endregion
+	}
 }
